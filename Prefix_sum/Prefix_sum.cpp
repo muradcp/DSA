da96d456cceec552090bda284aa42718c87d7da8
muradcp <eadris34@gmail.com>
2024-07-31 21:33:45 +0600

    Update Prefix_sum.cpp

--- Prefix_sum/Prefix_sum.cpp
+++ Prefix_sum/Prefix_sum.cpp
@@ -14,45 +14,39 @@ Describe:
         arr------ 1   6   4    2    5     3
         pref------1   7   11   13   18    21
 
-*/
 #include <bits/stdc++.h>
-#define nl '\n'
-#define ll long long
 using namespace std;
-#define vl vector<ll>
-vl psum(const vl &arr, ll n)
+
+using ll = long long;
+using vl = vector<ll>;
+
+vl psum(const vl &a)
 {
-    vl pref(n + 1);
-    // for(ll i=0;i<n;i++)
-    // {
-    //     pref[i]=pref[i-1]+arr[i];
-    // }
-    partial_sum(arr.begin(), arr.end(), pref.begin());
-    return pref;
-}
-void solve()
-{
-    ll n, t;
-    cin >> n >> t;
-    vl arr(n);
-    for (ll i = 0; i < n; i++)
-        cin >> arr[i];
-    vl narr = psum(arr, n);
-    while (t--)
-    {
-        ll l, r;
-        cin >> l >> r;
-        cout << narr[r] - narr[l] << nl;
-    }
+    ll sz = a.size();
+    vl psum(sz + 1);
+    for (int i = 0; i < sz; ++i)
+        psum[i + 1] = psum[i] + a[i];
+    // or partial_sum(begin(a),end(a),begin(psum)+1);
+    return psum;
 }
 int main()
 {
-    int tc = 1;
-    // cin>>tc;
-    for (int i = 1; i <= tc; i++)
+    int N, Q;
+    cin >> N >> Q;
+    vl a(N);
+    for (ll &x : a)
+        cin >> x;
+    vl p = psum(a);
+    for (auto it : p)
+        cout << it << " ";
+    cout << "\n";
+    for (int i = 0; i < Q; ++i)
     {
-        // cout<<"Case #"<<i<<": ";
-        solve();
+        int l, r;
+        cin >> l >> r;
+        if (l == 0)
+            cout << p[r] << "\n";
+        else
+            cout << p[r] - p[l] << "\n";
     }
-    return 0;
 }
