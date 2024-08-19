4ab6d37af7c9eadba30b7e77415338031fda4f4d
muradcp <eadris34@gmail.com>
2024-08-19 12:41:01 +0600

    Create Find_the_first_reapting_element.cpp

---
+++ Hashing/Find_the_first_reapting_element.cpp
@@ -0,0 +1,58 @@
+/*
+    Problem link: Find the first repeating element in an array
+
+    Input:
+            10 5 3 4 3 5 6
+
+    Output: 
+            5
+
+*/
+
+#include <bits/stdc++.h>
+#define nl '\n'
+#define ll long long
+using namespace std;
+#define vl vector<ll>
+const ll mod = 1000000007;
+void vdbg(vector<int> &arr)
+{
+    for (auto it : arr)
+        cout << it << " ";
+    cout << nl;
+}
+void dbg(ll n)
+{
+    cout << n << " ";
+    cout << nl;
+}
+void solve()
+{
+    ll n;
+    cin >> n;
+    vl arr(n);
+    for (ll i = 0; i < n; i++)
+        cin >> arr[i];
+    set<ll> s;
+    for (ll i = 0; i < n; i++)
+    {
+        if (s.find(arr[i]) != s.end())
+        {
+            cout << "YES" << nl;
+            cout<<arr[i]<<nl;
+            return;
+        }
+        s.insert(arr[i]);
+    }
+    cout << "NO" << nl;
+}
+int main()
+{
+    ll tc = 1;
+    // cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        solve();
+    }
+    return 0;
+}
